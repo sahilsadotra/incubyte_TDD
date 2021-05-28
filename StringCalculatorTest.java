b2abf276aa30aadd297367278fbdabbe7e943d46
Sahil Sadotra <sadotrasahil@gmail.com>
2021-05-28 16:03:17 +0530

    Update String Calculator Test

--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -1,12 +1,10 @@
 import org.junit.Test;
-
 import junit.framework.Assert;
-
 public class StringCalculatorTest {
-    
+	
 
     @Test
-    public final void whenAnyNumberOfNumbersUsed() {
-        Assert.assertEquals(3+6+15+18+46+33, StringCalculator.add("3,6,15,18,46,33"));
+    public final void whenDelimiterIsSpecified() {
+        Assert.assertEquals(1+2+5, StringCalculator.add("//;n1;2;5"));
     }
 }

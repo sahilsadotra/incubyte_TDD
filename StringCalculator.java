8c9a8a219c4923ca002163a7cef3f6b91e9c3a22
Sahil Sadotra <sadotrasahil@gmail.com>
2021-05-28 15:19:54 +0530

    Create String Calculator

---
+++ StringCalculator.java
@@ -0,0 +1,18 @@
+class StringCalculator {
+
+    public static int add(final String text) {
+
+        String[] stringArray = text.split(",");
+
+        if (stringArray.length > 2) {
+
+            throw new RuntimeException("Upto 2 numbers separated by , are allowed");
+        } else {
+            int sum = 0;
+            for (int i = 0; i < stringArray.length; i++) {
+                sum += Integer.parseInt(stringArray[i]);
+            }
+            return sum;
+        }
+    }
+}

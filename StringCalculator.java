0996ba0ea8cd12112b910e722f1c5c091a3db327
Sahil Sadotra <sadotrasahil@gmail.com>
2021-05-28 16:08:19 +0530

    Update String Calculator

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,17 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class StringCalculator {
+
+    public static void main(String args[]){
+
+        System.out.println(add("3,-6,15,-18,46,33"));
+    }
 
     public static int add(final String text) {
 
         int sum = 0;
+        List<Integer> negativeNumbers = new ArrayList();
         String delimiter = ",|n";
         String noWithoutDelimiter = text;
         String[] stringArray = text.split(",|n");
-
         if(stringArray.length == 0){
             return sum;
         }
-
-		
+        
 
         if (text.startsWith("//")) {
             int delimiterIndex = text.indexOf("//") + 2;
@@ -22,25 +29,21 @@ class StringCalculator {
 
         if(stringArray.length > 0){
             for (int i = 0; i < stringArray.length; i++) {
-                if(!stringArray[i].isEmpty()){
-                    sum += Integer.parseInt(stringArray[i]);
+
+                if (!stringArray[i].isEmpty()) {
+                    int numberInt = Integer.parseInt(stringArray[i]);
+                    if(numberInt < 0){
+                        negativeNumbers.add(Integer.parseInt(stringArray[i]));
+                    }
+                    sum += numberInt;
                 }
+
             }
+            if (negativeNumbers.size() > 0) {
+                throw new RuntimeException("Negatives not allowed: " + negativeNumbers.toString());
+            }
+
             return sum;
         }
 
         return sum;
-
-    }
-    private static int add(final String text, final String delimiter) {
-        // TODO Auto-generated method stub
-        int sum = 0;
-        String[] stringArray = text.split(delimiter);
-        for (int i = 0; i < stringArray.length; i++) {
-            if (!stringArray[i].isEmpty()) {
-                sum += Integer.parseInt(stringArray[i].trim());
-            }
-        }
-        return sum;
-    }
-}

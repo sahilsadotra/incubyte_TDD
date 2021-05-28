a984baf78b60e46927648227cb1a00dc14c57385
Sahil Sadotra <sadotrasahil@gmail.com>
2021-05-28 15:39:55 +0530

    Update String Calculator

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,20 +1,21 @@
 class StringCalculator {
-
     public static int add(final String text) {
-
         String[] stringArray = text.split(",");
-
         if (stringArray.length > 2) {
 
             throw new RuntimeException("Upto 2 numbers separated by , are allowed");
-        } else if (stringArray.length == 0) {
-            return 0;
-        } else {
+        } else if (stringArray.length > 0) {
+
             int sum = 0;
             for (int i = 0; i < stringArray.length; i++) {
-                sum += Integer.parseInt(stringArray[i]);
+                if (stringArray[i] != "") {
+                    sum += Integer.parseInt(stringArray[i]);
+                }
             }
             return sum;
+        } else {
+
+            return 0;
         }
     }
 }

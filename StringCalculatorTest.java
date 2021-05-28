ecf85389a51f4ac8cc271be89bf3e8019381b756
Sahil Sadotra <sadotrasahil@gmail.com>
2021-05-28 15:44:13 +0530

    Update String Calculator Test

--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -1,9 +1,12 @@
 import org.junit.Test;
-
 public class StringCalculatorTest {
-
     @Test(expected = RuntimeException.class)
     public final void whenMoreThanTwoNumberUsed(){
         StringCalculator.add("1,4,6,7");
+    }
+
+    @Test
+    public final void whenStringIsEmpty(){
+        Assert.assertEquals(0, StringCalculator.add(""));
     }
 }

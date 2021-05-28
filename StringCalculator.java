380b00c02458fc370348d1afa8b0a81bd4f2f547
Sahil Sadotra <sadotrasahil@gmail.com>
2021-05-28 15:53:27 +0530

    Update String Calculator Test

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,13 +1,10 @@
+
 class StringCalculator {
+
     public static int add(final String text) {
 
-        String[] stringArray = text.split(",");
-
-//		if (stringArray.length > 2) {
-//
-//			throw new RuntimeException("Upto 2 numbers separated by , are allowed");
-//		} else if (stringArray.length > 0) {
-
+        String[] stringArray = text.split(",|n");
+        
         int sum = 0;
         for (int i = 0; i < stringArray.length; i++) {
             if (stringArray[i] != "") {
@@ -15,9 +12,6 @@ class StringCalculator {
             }
         }
         return sum;
-//		} else {
-//
-//			return 0;
-//		}
+
     }
 }

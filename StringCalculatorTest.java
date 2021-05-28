a01d518c39f35c255536e8c9bf66c36d99c53214
Sahil Sadotra <sadotrasahil@gmail.com>
2021-05-28 16:09:42 +0530

    Update String Calculator Test

--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -1,10 +1,7 @@
 import org.junit.Test;
 import junit.framework.Assert;
-
 public class StringCalculatorTest {
-
-
-
+    
     @Test
     public final void whenStringIsEmpty(){
         Assert.assertEquals(0, StringCalculator.add(""));
@@ -23,5 +20,17 @@ public class StringCalculatorTest {
     @Test
     public final void whenDelimiterIsSpecified() {
         Assert.assertEquals(1+2, StringCalculator.add("//;\n1;2"));
+    }
+
+    @Test
+    public final void whenNegativeNumbersAreUsed() {
+        RuntimeException exception = null;
+        try {
+            StringCalculator.add("3,-6,15,-18,46,33");
+        } catch (RuntimeException e) {
+            exception = e;
+        }
+        Assert.assertNotNull(exception);
+        Assert.assertEquals("Negatives not allowed: [-6, -18]", exception.getMessage());
     }
 }
